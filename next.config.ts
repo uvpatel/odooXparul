777ea26aa5a241958acab7967d61bc2d0d700e73
Viveksinhchavda <24cp012@bvmengineering.ac.in>
2026-05-09 14:57:24 +0530

    implemente application

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.image2url.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
