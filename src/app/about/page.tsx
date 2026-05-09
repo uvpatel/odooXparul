dd5f939827dc761d2279c8f8e241bdd8a48c48a4
Viveksinhchavda <24cp012@bvmengineering.ac.in>
2026-05-09 15:58:04 +0530

    Added About page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import HeroSectionOne from '@/components/hero-section-demo-1'
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>
+
+      <HeroSectionOne />
+    </div>
+  )
+}
