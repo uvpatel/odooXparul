fec323362a672155b35266b368a9e6f62a2aad05
Viveksinhchavda <24cp012@bvmengineering.ac.in>
2026-05-09 15:07:29 +0530

    tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import BackgroundRippleEffectDemo from "@/components/background-ripple-effect-demo";
+import TabsDemo from "@/components/tabs-demo";
+import TeamShowcase from "@/components/team-section";
 
 export default function Home() {
   return (
-    
+
     <>
       <BackgroundRippleEffectDemo />
+      <TeamShowcase />
+      <TabsDemo />
     </>
   );
 }
