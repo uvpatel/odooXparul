13a464de3249c4c3f5fd598fc0a6079a1448dc69
Viveksinhchavda <24cp012@bvmengineering.ac.in>
2026-05-09 15:01:27 +0530

    changes in team section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import BackgroundRippleEffectDemo from "@/components/background-ripple-effect-demo";
+import TeamShowcase from "@/components/team-section";
 
 export default function Home() {
   return (
-    
+
     <>
       <BackgroundRippleEffectDemo />
+      <TeamShowcase />
     </>
   );
 }
