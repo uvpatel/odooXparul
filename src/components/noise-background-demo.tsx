74f53690c663f58d350dfd5adf3de60518cf161d
Viveksinhchavda <24cp012@bvmengineering.ac.in>
2026-05-10 16:19:59 +0530

    fix it

--- src/components/noise-background-demo.tsx
+++ src/components/noise-background-demo.tsx
@@ -1,4 +1,5 @@
 import { NoiseBackground } from "@/components/ui/noise-background";
+import Link from "next/link";
 
 export default function NoiseBackgroundDemo() {
   return (
@@ -12,7 +13,7 @@ export default function NoiseBackgroundDemo() {
         ]}
       >
         <button className="h-full w-full cursor-pointer rounded-full bg-linear-to-r from-neutral-100 via-neutral-100 to-white px-4 py-2 text-black shadow-[0px_2px_0px_0px_var(--color-neutral-50)_inset,0px_0.5px_1px_0px_var(--color-neutral-400)] transition-all duration-100 active:scale-98 dark:from-black dark:via-black dark:to-neutral-900 dark:text-white dark:shadow-[0px_1px_0px_0px_var(--color-neutral-950)_inset,0px_1px_0px_0px_var(--color-neutral-800)]">
-          Dashboard &rarr;
+          <Link href="/dashboard"> Dashboard &rarr;</Link>
         </button>
       </NoiseBackground>
     </div>
